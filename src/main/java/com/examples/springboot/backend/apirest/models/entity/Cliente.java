68f7700260e38931cdae1533ab9430833816ef6a
Oscar <oscarcr189@outlook.com>
2023-08-02 22:56:16 -0600

    clase cliente-2 spring

--- src/main/java/com/examples/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/examples/springboot/backend/apirest/models/entity/Cliente.java
@@ -3,15 +3,31 @@ package com.examples.springboot.backend.apirest.models.entity;
 import java.io.Serializable;
 import java.util.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
+@Entity
+@Table(name="clientes")
 public class Cliente implements Serializable{
 
-	
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nombre;
+	
 	private String apellido;
+	
 	private String email;
+	
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
 	private Date createAt;
 
 	public Long getId() {
