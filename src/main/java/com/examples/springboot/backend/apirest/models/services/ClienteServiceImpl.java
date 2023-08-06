3c71c917ffddbd6a30779fa3adff7dfd1360e22b
Oscar <oscarcr189@outlook.com>
2023-08-05 21:57:05 -0600

    añadiendo crud1

--- src/main/java/com/examples/springboot/backend/apirest/models/services/ClienteServiceImpl.java
+++ src/main/java/com/examples/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -22,4 +22,28 @@ public class ClienteServiceImpl implements IClientesService{
 		return (List<Cliente>)clienteDao.findAll();
 	}
 
+	
+	@Override
+	@Transactional(readOnly = true)
+	public Cliente findById(Long id) {
+		
+		return clienteDao.findById(id).orElse(null);
+	}
+	
+
+	@Override
+	@Transactional
+	public Cliente save(Cliente cliente) {
+		
+		return clienteDao.save(cliente);
+	}
+
+	
+	@Override
+	@Transactional
+	public void delete(Long id) {
+		
+		clienteDao.deleteById(id);
+	}
+
 }
--- src/main/java/com/examples/springboot/backend/apirest/models/services/IClientesService.java
+++ src/main/java/com/examples/springboot/backend/apirest/models/services/IClientesService.java
@@ -7,4 +7,12 @@ import com.examples.springboot.backend.apirest.models.entity.Cliente;
 public interface IClientesService {
 
 	public List<Cliente> findAll();
+	
+	public Cliente findById(Long id);
+	
+	public Cliente save(Cliente cliente);
+	
+	public void delete(Long id);
+	
+	
 }
