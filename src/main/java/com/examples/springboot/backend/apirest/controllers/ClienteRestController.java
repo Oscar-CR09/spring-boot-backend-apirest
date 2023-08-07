44a47fd199d656f5a314eae3521e5e431c1143b7
Oscar <oscarcr189@outlook.com>
2023-08-06 20:31:15 -0600

    añadiendo crud2

--- src/main/java/com/examples/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/examples/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,11 +1,16 @@
 package com.examples.springboot.backend.apirest.controllers;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.examples.springboot.backend.apirest.models.entity.Cliente;
@@ -25,5 +30,29 @@ public class ClienteRestController {
 	
 		return clienteService.findAll();
 	}
+	
+	@GetMapping("/clientes/{id}")
+	public Cliente show(@PathVariable Long id) {
+		return clienteService.findById(id);
+		
+	}
+	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente create(@RequestBody Cliente cliente) {
+		return clienteService.save(cliente);
+		
+	}
+	
+	@PutMapping("/clientes/{id}")
+	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente clienteActual = clienteService.findById(id);
+		
+		clienteActual.setApellido(cliente.getApellido());
+		clienteActual.setNombre(cliente.getNombre());
+		clienteActual.setEmail(cliente.getEmail());
+		
+		return clienteService.save(clienteActual);
+	}
+	
 	
 }
--- src/main/java/com/examples/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/examples/springboot/backend/apirest/models/entity/Cliente.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -29,7 +30,11 @@ public class Cliente implements Serializable{
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
-
+	
+	@PrePersist
+	public void prePersist() {
+		createAt =new Date();
+	}
 	public Long getId() {
 		return id;
 	}
