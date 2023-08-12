7cab2ae4564976eb4c95f09dc8f3c9ac0d232b9e
Oscar <oscarcr189@outlook.com>
2023-08-11 23:13:54 -0600

    añadiendo manejo de error create

--- src/main/java/com/examples/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/examples/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.hibernate.exception.DataException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
@@ -60,9 +61,20 @@ public class ClienteRestController {
 		
 	}
 	@PostMapping("/clientes")
-	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente create(@RequestBody Cliente cliente) {
-		return clienteService.save(cliente);
+	public ResponseEntity<?> create(@RequestBody Cliente cliente) {
+		
+		Cliente clienteNew = null;
+		Map<String, Object> response = new HashMap<>();
+		try {
+			clienteNew =  clienteService.save(cliente);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al realizar el insert en la base de datos");
+			response.put("mensaje",e.getMessage().concat(":").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String,Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR); 
+		}		
+		response.put("mensaje","El cliente ha sido creado con éxito!");
+		response.put("cliente", clienteNew);
+		return new ResponseEntity<Map<String,Object>>(response,HttpStatus.CREATED);
 		
 	}
 	
